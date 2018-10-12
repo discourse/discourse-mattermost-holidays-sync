ef325a55a9c6d59d96fa27e6015d761ee96957e0
Joshua Rosenfeld <jomaxro@discourse.org>
2018-10-12 10:11:25 -0400

    add url

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,7 @@
 # about: Updated mattermost usernames with `-v` for users on holiday
 # version: 0.1
 # author: David Taylor
+# url: https://github.com/vinothkannans/discourse-mattermost-holidays-sync
 
 after_initialize do
   require File.expand_path("../jobs/scheduled/update_mattermost_usernames", __FILE__)
