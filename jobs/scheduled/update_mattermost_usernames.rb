65b28c4b7b4f873f1a659505dafb1ff25d03e0a1
David Taylor <david@taylorhq.com>
2021-06-18 21:25:07 +0100

    FEATURE: Set mattermost status based on vacation status (#3)
    Optionally (default on) set users' mattermost status emoji/text when they are on vacation. The emoji is customizable via the site setting. If the user is not on vacation, and their status has this specific emoji, their status will be cleared.
    
    Optionally (default off) this job can clear ALL non-discourse-managed mattermost statuses

--- jobs/scheduled/update_mattermost_usernames.rb
+++ jobs/scheduled/update_mattermost_usernames.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Jobs
-  class UpdateMattermostUsernames < Jobs::Scheduled
+  class UpdateMattermostUsernames < ::Jobs::Scheduled
     every 10.minutes
 
     def execute(args)
@@ -9,41 +11,63 @@ module Jobs
 
       return if api_key.blank? || server.blank?
 
-      # Fetch all mattermost users
-      response = Excon.get("#{server}/api/v4/users", headers: {
+      headers = {
         "Authorization": "Bearer #{api_key}"
-      })
+      }
+
+      # Fetch all mattermost users
+      response = Excon.get("#{server}/api/v4/users", headers: headers)
       mattermost_users = JSON.parse(response.body, symbolize_names: true)
 
       # Loop over mattermost users
       mattermost_users.each do |user|
-        mattermost_username = user[:username]
-        marked_on_holiday = !!mattermost_username.chomp!("-v")
-
         discourse_user = User.find_by_email(user[:email])
+
         next unless discourse_user
         discourse_username = discourse_user.username
 
         on_holiday = users_on_holiday.include?(discourse_username)
 
-        update_username = false
-        if on_holiday && !marked_on_holiday
-          update_username = "#{mattermost_username}-v"
-        elsif !on_holiday && marked_on_holiday
-          update_username = mattermost_username
+        if SiteSetting.discourse_mattermost_suffix_usernames
+          mattermost_username = user[:username]
+          username_on_holiday = !!mattermost_username.chomp!("-v")
+
+          update_username = false
+          if on_holiday && !username_on_holiday
+            update_username = "#{mattermost_username}-v"
+          elsif !on_holiday && username_on_holiday
+            update_username = mattermost_username
+          end
+
+          if update_username
+            # puts "Update #{mattermost_username} to #{update_username}"
+            Excon.put("#{server}/api/v4/users/#{user[:id]}/patch",
+              headers: headers,
+              body: { username: update_username }.to_json
+            )
+          end
         end
 
-        if update_username
-          # puts "Update #{mattermost_username} to #{update_username}"
-          Excon.put("#{server}/api/v4/users/#{user[:id]}/patch", headers: {
-            "Authorization": "Bearer #{api_key}"
-          }, body: {
-            username: update_username
-          }.to_json)
-        end
+        if SiteSetting.discourse_mattermost_set_status
+          status_json = user.dig(:props, :customStatus)
+          status_data = status_json.present? ? JSON.parse(status_json, symbolize_names: true) : nil
+          status_emoji = status_data&.[](:emoji)
+          status_on_holiday = status_data&.[](:emoji) == SiteSetting.discourse_mattermost_holiday_status_emoji
+          purge_unknown_status = SiteSetting.discourse_mattermost_holiday_status_exclusive
 
+          if on_holiday && !status_on_holiday
+            Excon.put("#{server}/api/v4/users/#{user[:id]}/status/custom",
+              headers: headers,
+              body: {
+                emoji: SiteSetting.discourse_mattermost_holiday_status_emoji,
+                text: SiteSetting.discourse_mattermost_holiday_status_text
+              }.to_json
+            )
+          elsif !on_holiday && (status_on_holiday || (purge_unknown_status && status_emoji))
+            Excon.delete("#{server}/api/v4/users/#{user[:id]}/status/custom", headers: headers)
+          end
+        end
       end
-
     end
 
   end
